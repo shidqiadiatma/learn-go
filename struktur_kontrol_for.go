fcf9abe0280a92990b7272d72e734269d405e26f
shidqiadiatma <adiatma944@gmail.com>
2023-03-10 19:26:02 +0700

    chapter 1 sesi 2

--- struktur_kontrol_for.go
+++ struktur_kontrol_for.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	for i := 0; i < 5; i++ {
+		fmt.Println("Nilai i = ", i)
+	}
+	var j int = 0
+	for j < 5 {
+		fmt.Println("Nilai j = ", j)
+		j++
+	}
+	for pos, char := range "САШАРВО" {
+		fmt.Printf("character %#U starts at byte position %d\n", char, pos)
+	}
+	for {
+		if j++; j <= (10) {
+			fmt.Println("Nilai j = ", j)
+		} else {
+			break
+		}
+	}
 }
