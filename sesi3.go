2cd94f4bce9d6a10af5dfa39f07dac6fda9162fd
shidqiadiatma <adiatma944@gmail.com>
2023-03-15 19:18:44 +0700

    chapter 1 sesi 3

--- sesi3.go
+++ sesi3.go
@@ -7,39 +7,30 @@ import (
 )
 
 func main() {
-	// result text
-	calculateWords("selamat malam")
+	calculateWords("selamat malam") // result text
 }
 
 func calculateWords(text string) {
-	// variabel awal dan akhir
-	elementMapAwal, elementMapAkhir := map[string]int{}, make(map[string]int)
+	elementMapAwal, elementMapAkhir := map[string]int{}, make(map[string]int) // variabel awal dan akhir
 
-	// dipotong menjadi slice
-	pecahPerkata := strings.Split(text, "")
+	pecahPerkata := strings.Split(text, "") // dipotong menjadi slice
 
-	// di looping simpan ke variabel awal
-	for _, w := range pecahPerkata {
+	for _, w := range pecahPerkata { // di looping simpan ke variabel awal
 		fmt.Println(w)
 		elementMapAwal[w]++
 	}
 
 	keys := make([]string, 0, len(elementMapAwal))
 
-	//for looping simpan ke variabel keys
-	for k := range elementMapAwal {
+	for k := range elementMapAwal { //for looping simpan ke variabel keys
 		keys = append(keys, k)
 	}
 
-	// mengurutkan dari a sampai z
-	sort.Strings(keys)
+	sort.Strings(keys) // mengurutkan dari a sampai z
 
-	// menyimpan variabel akhir
-	for _, k := range keys {
-		//fmt.Printf("%s = %d\n", k, elementMapAwal[k])
-		elementMapAkhir[k] = elementMapAwal[k]
+	for _, k := range keys { // menyimpan variabel akhir
+		elementMapAkhir[k] = elementMapAwal[k] //fmt.Printf("%s = %d\n", k, elementMapAwal[k])
 	}
 
-	// menampilkan hasil output
-	fmt.Println(elementMapAkhir)
+	fmt.Println(elementMapAkhir) // menampilkan hasil output
 }
